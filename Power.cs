8403da57d03698d6b477958e2f156802f0c16296
kumarharmesh <kumarharmesh@gmail.com>
2020-08-24 22:23:19 +0530

    Create Power.cs

---
+++ Power.cs
@@ -0,0 +1,14 @@
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            int x,y;
+            double power;
+            Console.WriteLine("Enter first number:");
+            x=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter first number:");
+            y=Convert.ToInt32(Console.ReadLine());
+            power=Math.Pow(x,y);
+            Console.WriteLine($"{x}  power {y} = {power}");
+        }
+    }
